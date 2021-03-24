e964f28d011f8fb84d3522c69f36871c63442616
Amr Hashem <47293199+AmrHash@users.noreply.github.com>
2021-03-24 11:34:49 +0200

    s

--- .history/src/dj_pd/settings_20210324113414.py
+++ .history/src/dj_pd/settings_20210324113414.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k-a1fi8&73uy6cf4-ng68ldc_41e$w%me5jee6gmb#_0w&x3f7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
